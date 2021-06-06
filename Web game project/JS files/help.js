fdeb8d693ef1a074dfbd9e4690d9778b928e216e
TheBoldReaper <82278824+TheBoldReaper@users.noreply.github.com>
2021-06-06 22:07:28 +0800

    Add files via upload

---
+++ Web game project/JS files/help.js
@@ -0,0 +1,15 @@
+const languages = document.getElementById('changelanguage')
+
+
+languages.addEventListener("click", () => {
+    console.log("hello world!")
+    document.getElementById('changelanguage').innerHTML = "Current Language - Malay"
+}
+)
+
+
+
+
+
+
+
